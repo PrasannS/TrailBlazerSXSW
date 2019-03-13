e114849b8edf2f22bcfedd3dc9d046f2fc1c3bf2
PrasannS <singhal.prasann07@gmail.com>
2019-03-13 06:23:40 -0500

    list opening setup

--- app/src/main/java/stringcheesedevs/android/apps/com/trailblazer/ToursActivity.java
+++ app/src/main/java/stringcheesedevs/android/apps/com/trailblazer/ToursActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -30,11 +32,17 @@ public class ToursActivity extends Activity {
         String artistname = intent.getStringExtra("artist");
         String artistCode = NamesUtils.artistIDs[Arrays.binarySearch(NamesUtils.names, artistname)];
 
-        ArrayAdapter adapter = new ArrayAdapter<>(this,
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 R.layout.listview, getTourNames());
 
         ListView listView = (ListView) findViewById(R.id.mobile_list);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                openTour(getTourNames()[position]);
+            }
+        });
     }
 
     public void setTours(String artistname) {
@@ -49,5 +57,11 @@ public class ToursActivity extends Activity {
             cur++;
         }
         return as;
+    }
+
+    public void openTour(String tourname){
+        Intent intent = new Intent(this,TourActivity.class);
+        intent.putExtra("tourname",tourname);
+        startActivity(intent);
     }
 }
